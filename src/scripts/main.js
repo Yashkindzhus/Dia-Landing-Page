0ea6da2d4ff5d9f082dd96c6b1fb81a22cf0ff14
anastasiia <theyashkindzhus@gmail.com>
2021-10-19 15:16:24 +0300

    edit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,12 +8,10 @@ let position = 0;
 
 move(0);
 
-// Ждём клик от пользователя на кнопке Next
 next.addEventListener('click', function() {
   move(1);
 });
 
-// Ждём клик от пользователя на кнопке Prev
 prev.addEventListener('click', function() {
   move(-1);
 });
@@ -35,12 +33,10 @@ const body = document.querySelector('.page__body');
 const menuOpener = document.querySelector('.menu-toggler--opener');
 const menu = document.querySelector('.menu');
 
-// We disable page scroll when openning the menu
 menuOpener.addEventListener('click', function() {
   body.classList.add('page__body--with-menu');
 });
 
-// We enable page scrolling after closing the menu or pressing a nav link
 menu.addEventListener('click', () => {
   body.classList.remove('page__body--with-menu');
 });
